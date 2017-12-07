1e339fbafb65d6699926f1cddc95396c5d69b3eb
Gunnvant Singh <gunnvant3.14@gmail.com>
2017-12-07 10:36:48 +0530

    print statements fixed

--- Codes/Py DataStructures.py
+++ Codes/Py DataStructures.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 """
 Created on Fri Jun 17 12:18:00 2016
 
@@ -28,17 +28,17 @@ print type(b)
 
 a1=3
 a2=6
-print a1/a2
-print type(a1/a2)
+print(a1/a2)
+print(type(a1/a2))
 
 a1=3
 a2=6
-print a1/float(a2)
-print type(a1/float(a2))
+print(a1/float(a2))
+print(type(a1/float(a2)))
 
 #We can know about the methods available in the respective python classes by using the dir() method
-print dir(a)
-print dir(b)
+print(dir(a))
+print(dir(b))
 
 ### Strings
 #- Strings as iterables
@@ -46,34 +46,34 @@ print dir(b)
 #- String methods
 
 st='Jigsaw'
-print st[0]
-print st[1]
-print st[0:4]
-print st[-1]
+print(st[0])
+print(st[1])
+print(st[0:4])
+print(st[-1])
 
-print dir(st)
+print(dir(st))
 
-print len(st)
-print st.count('i')
-print st.find('i')
-print st.find('l')
+print(len(st))
+print(st.count('i'))
+print(st.find('i'))
+print(st.find('l'))
 
 w="Two Shōkaku-class aircraft carriers, Shōkaku and Zuikaku, were commissioned by the Imperial Japanese Navy during World War II. They participated in the attack on Pearl Harbor, the Indian Ocean Raid, and the battles of the Coral Sea, the Eastern Solomons, and the Santa Cruz Islands. Their air groups sank two of the four fleet carriers lost by the United States Navy during the war in addition to one elderly British light carrier. Returning to Japan after the Battle of the Coral Sea to repair damage and replace lost aircraft, they missed the Battle of Midway in June 1942. After the catastrophic loss of four carriers during that battle, they formed the bulk of Japan's carrier force for the rest of the war. Shōkaku was sunk by an American submarine during the Battle of the Philippine Sea in June 1944 as the Americans invaded the Mariana Islands, and Zuikaku was sacrificed as part of a decoy force four months later in the Battle of Leyte Gulf, both with heavy loss of life. Historian Mark Peattie called them 'arguably the best aircraft carriers' of the early 1940s."
 
-print w
+print(w)
 
 ### Looping and conditionals interlude
 #- Using for loops and if-else statements
 
 # All iterables can be looped through
 for c in w[0:20]:
-      print c
+      print(c)
      
 # We can control the flow of execution of code by using conditionals such as if-else
 if 2>3: #Notice that a statement is followed by a :
-    print 'Oh boy!!!'
+    print('Oh boy!!!')
 else:
-    print 'Yay!!'
+    print('Yay!!')
     
     
 ### Class Assignment
@@ -94,14 +94,14 @@ assign_txt="Narendra Damodardas Modi, born 17 September 1950) is the 15th and cu
 
 #Lists are the most commonly used data structure
 l1=[1,2,3,4,5,6]
-print l1
-print l1[0]
-print l1[0:3]
+print(l1)
+print(l1[0])
+print(l1[0:3])
 
-print dir(l1)
+print(dir(l1))
 
 l1.append(7)
-print l1
+print(l1)
 
 ### Class assignment
 # Use the assign_txt
@@ -121,14 +121,14 @@ b=['a','b','c','d']
 [[x,y] for x in a for y in b if a.index(x)==b.index(y)]
 
 c=range(11)
-print c
+print(c)
 
 #Suppose we want to create a list containing the squares in the list c
 #Method 1
 s=[]
 for num in c:
     s.append(num**2)
-print s
+print(s)
 
 #Method 2
 [x**2 for x in c ] #Very concise way
@@ -140,15 +140,15 @@ mass=[45,55,65,76]
 ht=[1.65,1.70,1.55,1.80]
 bmi=map(lambda x,y:x/y**2,mass,ht)
 bmi_f=filter(lambda x:x<20,bmi)
-print bmi
-print bmi_f
+print(bmi)
+print(bmi_f)
 su=reduce(lambda x,y:x+y,bmi)
-print su
+print(su)
 
 #Using list comprehension to create equivalent results
 #bmi
 BMI=[x/y**2 for x in mass for y in ht if mass.index(x)==ht.index(y)]
-print BMI
+print(BMI)
 #filter
 [x for x in BMI if x<20]
 
@@ -158,18 +158,18 @@ print BMI
 # Tuple methods
 
 t=(1,2,3,5)
-print t
+print(t)
 
-print t[0]
-print t[1]
+print(t[0])
+print(t[1])
 
 li6=[1,2,3,4]
 li6[0]=0
-print li6
+print(li6)
 
 t[0]=0
 
-print dir(t)
+print(dir(t))
 
 # zip
 a=[1,2,3,4]
@@ -185,27 +185,27 @@ zip(a,b)
 # Looping through a dictionary
 
 di={'Abi_wt':100, 'Abi_sleeps': 16}
-print di
-print dir(di)
+print(di)
+print(dir(di))
 
-print di.values()
-print di.keys()
+print(di.values())
+print(di.keys())
 
-print di.items()
+print(di.items())
 
 sorted(di.iteritems())
 
 di['Abi_sleeps']=20
-print di
+print(di)
 
 'Abi_wt' in di
 20 in di#We can check if particular keys exist using simple 'in' statement
 
 for x in di:
-    print x
+    print(x)
     
 for x in di:
-    print x, di[x]
+    print(x, di[x])
     
 ### Use the assign_txt
 # Find out the occurence of each word in text string (Create a list of each word in the string, loop over this list to create a dictionary of words and their occurences
@@ -221,8 +221,8 @@ os.chdir('F:\\Work\\Python')
 
 f=open('tweets_assignment.txt','r')
 fo=f.read()
-print type(f)
-print type(fo)
+print(type(f))
+print(type(fo))
 f.close()
 
 f=open('write.txt','w')
